c303d6b55382ddb6045043e4e5afdeb713d7a7d0
Allan Cantin <allan.cantin@epita.fr>
2020-03-03 12:14:31 +0100

    Projet database SmartLink

---
+++ Database/dbo/Tables/T_Shortcuts.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[T_Shortcuts] (
+    [id]        INT           NOT NULL,
+    [url]       VARCHAR (MAX) NOT NULL,
+    [hash]      VARCHAR (MAX) NOT NULL,
+    [sessionId] INT           NOT NULL,
+    CONSTRAINT [PK_T_Shortcuts] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ Database/dbo/Tables/T_Stats.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[T_Stats] (
+    [id]    INT      NOT NULL,
+    [idUrl] INT      NOT NULL,
+    [date]  DATETIME NOT NULL,
+    CONSTRAINT [PK_T_Stats] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_T_Stats_T_Shortcuts] FOREIGN KEY ([idUrl]) REFERENCES [dbo].[T_Shortcuts] ([id])
+);
+
