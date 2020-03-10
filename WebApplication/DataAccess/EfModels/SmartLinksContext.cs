51bc1b5319e94987bf834812d70b27a6a27e718c
Allan Cantin <allan.cantin@epita.fr>
2020-03-10 12:27:13 +0100

    Suppression des fichiers inutiles

--- WebApplication/DataAccess/EfModels/SmartLinksContext.cs
+++ WebApplication/DataAccess/EfModels/SmartLinksContext.cs
@@ -16,18 +16,17 @@ namespace WebApplication.DataAccess.EfModels
         }
 
         public virtual DbSet<StatByUrl> StatByUrl { get; set; }
-        
         public virtual DbSet<TShorcuts> TShorcuts { get; set; }
         public virtual DbSet<TStats> TStats { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Data Source=.\\MTI;Initial Catalog=SmartLink;Trusted_Connection=True");
             }
-        }
+        }*/
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
