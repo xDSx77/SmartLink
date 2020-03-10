746dac3e06ff31299e4754236c1cea2af2b1cf14
Allan Cantin <allan.cantin@epita.fr>
2020-03-10 11:39:51 +0100

    Déplacement de la connection string de la db vers le fichier de config appsettings.json + ajout du context dans l'injection de dépendance

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApplication.DataAccess.EfModels;
 
 namespace WebApplication
 {
@@ -24,6 +26,9 @@ namespace WebApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<SmartLinkContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
